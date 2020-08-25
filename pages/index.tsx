7381f2d8a14287cff9f211354d0f9f8c5558a661
Keisuke SATO <riaf@me.com>
2020-08-25 11:46:22 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,41 +12,18 @@ import {
 export default () => (
   <Page title="U-16プログラミングコンテスト釧路大会">
     <Jumbotron>
-      <JumbotronHead>2019 第7回大会</JumbotronHead>
+      <JumbotronHead>2020 第8回大会</JumbotronHead>
       <JumbotronLead>
-        10月12日 (土) 釧路市生涯学習センター まなぼっと 多目的ホール
+        10月25日 (日) オンライン開催
       </JumbotronLead>
     </Jumbotron>
 
     <section>
-      <Title>お詫び</Title>
+      <Title>2020オンライン開催決定</Title>
       <Text>
-        今年は都合により事前講習会の用意ができませんでした。楽しみにしていてくださったみなさまには大変申し訳ありませんでした。
-      </Text>
-      <Text>
-        開催につきましては、午前中に直前の講習会、午後の時間で対戦というような半ば体験会のような形となります。
-      </Text>
-      <Text>
-        当日は10時ころから直前の講習会を実施し、14時ころより対戦を行う予定です。
-      </Text>
-      <Text>
-        過去に講習会や大会への参加経験があればぜひご参加ください。
-        <br />
-        また経験が無くてもプログラミングに興味があれば、高専生を中心にサポートいたします。
-        <br />
-        ご参加お待ちしております。
+        8/31からオンラインでの講習会の開催を予定しています。詳細決定次第更新予定です。
       </Text>
     </section>
-
-    <Contact>
-      <ContactButton
-        href="https://forms.gle/dZ2qdH7ntjWDMSdC6"
-        target="_blank"
-        size="x-large"
-      >
-        参加申し込みフォーム
-      </ContactButton>
-    </Contact>
 
     <Contact>
       <ContactButton
